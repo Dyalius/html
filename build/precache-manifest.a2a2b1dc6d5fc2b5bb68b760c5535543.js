6e54269f5fb3f1e97760bb946797d602728505ae
Dyalius Drew <dyalius.drew@gmail.com>
2018-12-03 15:29:28 -0800

    build

---
+++ build/precache-manifest.a2a2b1dc6d5fc2b5bb68b760c5535543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f479f8327ee53631898d",
+    "url": "/static/js/main.f479f832.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "f479f8327ee53631898d",
+    "url": "/static/css/main.2731ae69.chunk.css"
+  },
+  {
+    "revision": "4f2d03939746ba3a8b4d157ed66bb73f",
+    "url": "/index.html"
+  }
+];
